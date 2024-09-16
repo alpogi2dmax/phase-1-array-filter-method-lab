3019fb41b9fd2c1b45df9a0699aea80501e64b66
Al Cerdan Lico <alcerdan@yahoo.com>
2024-09-16 12:31:26 -0700

    completed array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection, name) {
+    return collection.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(collection, letters) {
+    return collection.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(collection, name) {
+    return collection.filter(driver => driver.name === name);
+}
